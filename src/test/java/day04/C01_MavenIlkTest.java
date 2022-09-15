263c264165c989a8172435a3f2dc2804c47be548
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-09-15 11:11:43 +0300

    14 Eylul

---
+++ src/test/java/day04/C01_MavenIlkTest.java
@@ -0,0 +1,59 @@
+package day04;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C01_MavenIlkTest {
+    public static void main(String[] args) {
+        /*
+       1- https://www.amazon.com/ sayfasina gidelim
+       2- arama kutusunu locate edelim
+       3- “Samsung headphones” ile arama yapalim
+       4- Bulunan sonuc sayisini yazdiralim
+       5- Ilk urunu tiklayalim
+       6- Sayfadaki tum basliklari yazdiralim
+     */
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        // 1- https://www.amazon.com/ sayfasina gidelim
+        driver.get("https://www.amazon.com/");
+
+        //2- arama kutusunu locate edelim
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+
+        //3- “Samsung headphones” ile arama yapalim
+        aramaKutusu.sendKeys("Samsung headphones", Keys.ENTER);
+
+        //4- Bulunan sonuc sayisini yazdiralim
+        //WebElement sonucYazisi = driver.findElement(By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        //System.out.println(sonucYazisi.getText());
+        String [] sonucYazisi= driver.findElement
+                (By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]")).
+                getText().split(" ");
+        System.out.println("Samsung headphones icin arama sonucu : " +sonucYazisi[2]);
+
+
+        //5- Ilk urunu tiklayalim
+        WebElement ilkUrun=driver.findElement(By.xpath("//*[@class='s-image'][1]"));
+        ilkUrun.click();
+
+        //6- Sayfadaki tum basliklari yazdiralim
+        System.out.println(driver.findElement(By.xpath("//h1")).getText());
+
+
+
+
+
+    }
+}
---
+++ src/test/java/day05/C04_JunitCheckBox.java
@@ -0,0 +1,67 @@
+package day05;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C04_JunitCheckBox {
+
+    /*
+      1. Bir class oluşturun : CheckBoxTest
+      2. Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
+      a. Verilen web sayfasına gidin.
+      https://the-internet.herokuapp.com/checkboxes
+      b. Checkbox1 ve checkbox2 elementlerini locate edin.
+      c. Checkbox1 seçili değilse onay kutusunu tıklayın
+      d. Checkbox2 seçili değilse onay kutusunu tıklayın
+     */
+
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+
+        //a. Verilen web sayfasına gidin.
+        // https://the-internet.herokuapp.com/checkboxes
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        // b. Checkbox1 ve checkbox2 elementlerini locate edin.
+        WebElement checkboxes1=driver.findElement(By.xpath("//*[@type='checkbox'][1]"));
+        WebElement checkboxes2=driver.findElement(By.xpath("//*[@type='checkbox'][2]"));
+
+       // c. Checkbox1 seçili değilse onay kutusunu tıklayın
+        Thread.sleep(2000);
+        if(!checkboxes1.isSelected()){
+            checkboxes1.click();
+        }
+
+       // d. Checkbox2 seçili değilse onay kutusunu tıklayın
+        Thread.sleep(2000);
+        if(!checkboxes2.isSelected()){
+            checkboxes2.click();
+        }
+
+
+    }
+
+}
