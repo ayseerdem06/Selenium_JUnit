92ccbc6edcbf07e4244a208807ce53eae502b74d
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-09-16 23:22:48 +0300

    16 Eylul

---
+++ src/test/java/day07/C01_Assertions.java
@@ -0,0 +1,45 @@
+package day07;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+public class C01_Assertions {
+    /*
+      Bir Class olusturalim YanlisEmailTesti
+      http://automationpractice.com/index.php sayfasina gidelim
+      Sign in butonuna basalim
+      Email kutusuna @isareti olmayan bir mail yazip enter’a
+      bastigimizda “Invalid email address” uyarisi ciktigini test edelim
+     */
+    WebDriver driver;
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @After
+    public void tearDown(){
+        //driver.close();
+    }
+    @Test
+    public void test1(){
+        //http://automationpractice.com/index.php sayfasina gidelim
+        driver.get("http://automationpractice.com/index.php");
+        //      Sign in butonuna basalim
+        driver.findElement(By.xpath("//*[@class='login']")).click();
+        //      Email kutusuna @isareti olmayan bir mail yazip enter’a
+        WebElement emailKutusu = driver.findElement(By.xpath("//*[@id='email_create']"));
+        emailKutusu.sendKeys("erol.evren.gmail.com");
+        //      bastigimizda “Invalid email address” uyarisi ciktigini test edelim
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Invalid email address.']")).isDisplayed());
+    }
+}
---
+++ src/test/java/day07/C04_.java
@@ -0,0 +1,56 @@
+package day07;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class C04_ {
+    /*
+    Bir class oluşturun: DropDown
+    https://the-internet.herokuapp.com/dropdown adresine gidin.
+    Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
+    Value kullanarak Seçenek 2'yi (Option 2) seçin ve yazdırın
+    Visible Text(Görünen metin) kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
+    Tüm dropdown değerleri(value) yazdırın
+    Dropdown’un boyutunu bulun, Dropdown’da 4 öğe varsa konsolda True , degilse
+    False yazdırın.
+
+     */
+
+    WebDriver driver;
+
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+    }
+
+    @After
+    public void tearDown(){
+        //driver.close();
+    }
+
+    @Test
+    public void test(){
+        //https://the-internet.herokuapp.com/dropdown adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+
+        //Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
+        WebElement ddm=driver.findElement(By.xpath("//*[@id='dropdown']"));
+        Select select=new Select(ddm);
+        select.selectByIndex(1);
+    }
+
+}
